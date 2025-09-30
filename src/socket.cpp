5ea5d3e7aa418ac20832435f9c85ccde7e805baa
Aleksander Tsarenko <alex2@macstudio.local>
2025-10-01 02:39:16 +0300

    fix on socket close

--- src/socket.cpp
+++ src/socket.cpp
@@ -200,13 +200,19 @@ class dap::Socket::Shared : public dap::ReaderWriter {
     }
   }
 
-  size_t read(void* buffer, size_t bytes) {
+    size_t read( void* buffer, size_t bytes )
+    {
     RLock lock(mutex);
     if (s == InvalidSocket) {
-      return 0;
+        return 0;
     }
-    auto len =
-        recv(s, reinterpret_cast<char*>(buffer), static_cast<int>(bytes), 0);
+    auto len = recv( s, buffer, int(bytes), 0 );
+
+    if ( len <= 0 )
+    {
+        close();
+    }
+
     return (len < 0) ? 0 : len;
   }
 
